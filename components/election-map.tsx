7710732bbf16d619915ec54f0d5121d59658c014
zykcode <zyskson@gmail.com>
2024-08-22 13:07:58 +0100

    feat: your commit message

--- components/election-map.tsx
+++ components/election-map.tsx
@@ -3,40 +3,31 @@
 import { geoMercator, geoPath } from 'd3-geo';
 import type { FeatureCollection } from 'geojson';
 import type { FC } from 'react';
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 interface MapProps {
   map: FeatureCollection;
 }
 
 const ElectionMap: FC<MapProps> = ({ map }) => {
-  const initialProjection = () => geoMercator().fitSize([100, 100], map);
-  const [projection, setProjection] = useState(initialProjection);
-  const path = geoPath().projection(projection);
-
   const containerRef = useRef<SVGSVGElement>(null);
 
-  useEffect(() => {
-    function handleResize() {
-      setProjection(initialProjection);
-    }
+  const initialProjection = () => geoMercator().fitSize([100, 100], map);
 
-    window.addEventListener('resize', handleResize);
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
+  const [projection] = useState(initialProjection);
+  const path = geoPath().projection(projection);
 
   return (
     <svg ref={containerRef} viewBox="0 0 100 100">
-      {map.features.map((feature, i) => {
-        return (
-          <g key={`${feature.properties!.lganame}${i}`} className="regions">
-            <path d={path(feature)!} stroke="#333" strokeWidth={0.5} />
-            );
-          </g>
-        );
-      })}
+      {map.features.map((feature, i) => (
+        <g key={`${feature.properties?.lganame ?? i}`} className="regions">
+          <path
+            d={path(feature) || undefined}
+            stroke="#333"
+            strokeWidth={0.5}
+          />
+        </g>
+      ))}
     </svg>
   );
 };
